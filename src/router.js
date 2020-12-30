942d149c21d5e7049c734b7e8e30befdacd62e5e
Oyasumy <xuancuong99.it@gmail.com>
2020-12-30 12:04:33 +0700

    st3

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import ErrorPage from "./component/ErrorPage";
 import HomePage from "./component/Home";
 import ListProduct from "./component/ListProduct/index";
 import Personal from "./component/Personal";
-
+const homePage="/bs-cilent-p3";
 const route = [
   {
     path: "/",
@@ -12,32 +12,32 @@ const route = [
     exact: true,
   },
   {
-    path: "/bs-cilent-p3",
+    path: `${homePage}`,
     main: HomePage,
     exact: true,
   },
   {
-    path: "/detail-product",
+    path: `${homePage}/detail-product`,
     main: DetailProduct,
     exact: false,
   },
   {
-    path: "/list-product",
+    path: `${homePage}/list-product`,
     main: ListProduct,
     exact: false,
   },
   {
-    path: "/list-cart",
+    path: `${homePage}/list-cart`,
     main: Cart,
     exact: false,
   },
   {
-    path: "/personal",
+    path: `${homePage}/personal`,
     main: Personal,
     exact: false,
   },
   {
-    path: "/error",
+    path: `${homePage}/error`,
     main: ErrorPage,
     exact: false,
   },
